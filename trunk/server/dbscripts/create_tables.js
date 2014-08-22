0e1b225a8aa17bf967d9498a4e536a467dc71202
Gopi Donthireddy <gopi.don@gmail.com>
2014-08-22 10:51:03 +0530

    add listing

--- trunk/server/dbscripts/create_tables.js
+++ trunk/server/dbscripts/create_tables.js
@@ -1,3 +1,10 @@
 /**
  * Created by gopi on 8/22/14.
  */
+var app = require('../server');
+var dataSource = app.dataSources.t2spare;
+var Listing = app.models.Listing;
+
+dataSource.automigrate('Listing', function (err) {
+    console.log("Created Listing table");
+});
