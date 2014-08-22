195d21293386a8eeb5490196ee7eaab6f9d017d6
Gopi Donthireddy <gopi.don@gmail.com>
2014-08-22 18:37:51 +0530

    add listing IP

--- trunk/server/dbscripts/create_tables.js
+++ trunk/server/dbscripts/create_tables.js
@@ -5,6 +5,7 @@ var app = require('../server');
 var dataSource = app.dataSources.t2spare;
 var Listing = app.models.Listing;
 var Listing_Comments = app.models.Listing_Comments;
+var Listing_IP = app.models.Listing_IP;
 
 dataSource.automigrate('Listing', function (err) {
     console.log("Created Listing table");
@@ -12,4 +13,8 @@ dataSource.automigrate('Listing', function (err) {
 
 dataSource.automigrate('Listing_Comments', function (err) {
     console.log("Created Listing_Comments table");
+});
+
+dataSource.automigrate('Listing_IP', function (err) {
+    console.log("Created Listing IP table");
 });
