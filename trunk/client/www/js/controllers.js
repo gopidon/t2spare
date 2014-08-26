36e5fd710b5a95588a7dc33e38a33347783af863
Gopi Donthireddy <gopi.don@gmail.com>
2014-08-26 21:16:59 +0530

    ACL

--- trunk/client/www/js/controllers.js
+++ trunk/client/www/js/controllers.js
@@ -1,9 +1,18 @@
-angular.module('t2spare.controllers', [])
+angular.module('t2spare.controllers', ['lbServices'])
 
-.controller('DashCtrl', ['$scope','URLConstants','$window','$location',function($scope, URLConstants, $window, $location) {
+.controller('DashCtrl', ['$scope','URLConstants','$window','$location','User','$http','$rootScope', 'Global','LocalStorage',function($scope, URLConstants, $window, $location, User, $http, $rootScope, Global, LocalStorage) {
+        $scope.global = Global;
+        $scope.getAuthenticated = function(){
+            return LocalStorage.get("authenticated", false);
+        }
         $scope.FBAuthURL = URLConstants.FBAuthURL;
-        var loginWindow,close, hasUserId, userId;
+        var loginWindow,close, hasUserId, userId, accessToken, userStr, accessTokenStr;
         $scope.openFB = function(){
+            /*$http.defaults.headers.common['Authorization'] = '887RdUdAjdpGlydXY1rHhdfQcYVqlD7YttpIwoE2baJywLff2bPmi27Wmv1NNkvw';
+            User.findById({id: 1}, function(data){
+                console.log("Fetched data");
+                console.log(JSON.stringify(data));
+            });*/
             loginWindow = $window.open($scope.FBAuthURL,'_blank','location=yes');
             loginWindow.addEventListener('loadstart', function(event){
                 console.log(event.url);
@@ -11,18 +20,30 @@ angular.module('t2spare.controllers', [])
                 hasUserId = event.url.indexOf("userId");
                 console.log("Has UserId:"+hasUserId);
                 if(close > -1 && hasUserId > -1){
-                    userId = event.url.match('userId=[^#]*');
-                    var userStr = userId[0];
-                    console.log(typeof userStr);
-                    console.log(userStr.indexOf('='));
-                    console.log(userStr.length);
-                    var res = userStr.substring(userStr.indexOf('=')+1);
-                    console.log(res);
+                    userId = event.url.match('userId=[^&]*');
+                    accessToken = event.url.match('accessToken=[^#]*');
+                    userStr = userId[0];
+                    accessTokenStr = accessToken[0];
+                    userId = userStr.substring(userStr.indexOf('=')+1, userStr.length);
+                    console.log(userStr);
+                    console.log(accessTokenStr);
+                    console.log(userId);
+                    accessToken = accessTokenStr.substring(accessTokenStr.indexOf('=')+1, accessTokenStr.length);
+                    console.log(accessToken);
+
+                    /*$scope.$apply(function(){
+                        $scope.global.user.id = userId;
+                        $scope.global.authenticated = true;
+                    })*/
+                    $scope.$apply(function(){
+                        LocalStorage.set("authenticated", true);
+                    });
+
                     loginWindow.close();
                 }
-               // console.log(loginWindow);
 
-            })
+
+            });
         }
 }])
 
@@ -36,3 +57,5 @@ angular.module('t2spare.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
 });
+
+//curl -X GET -H "Authorization: 887RdUdAjdpGlydXY1rHhdfQcYVqlD7YttpIwoE2baJywLff2bPmi27Wmv1NNkvw" http://localhost:3000/api/Users/1
--- trunk/client/www/js/services.js
+++ trunk/client/www/js/services.js
@@ -1,26 +1,37 @@
 angular.module('t2spare.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+    .factory("Global", [
+        function() {
+            var _this = this;
+            _this._data = {
+                user: {},
+                authenticated: false,
+                dateFormat: 'dd-MMMM-yyyy',
+                dateTimeFormat: 'dd-MMMM-yyyy HH:mm',
+                dateTimeFormatMeridian: 'dd-MMMM-yyyy hh:mm a',
+                timeFormat: 'HH:mm',
+                timeFormatMeridian: 'hh:mm a',
+                hourStep: 1,
+                minStep: 15,
+                isMeridian: true
+            };
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});
+            return _this._data;
+        }
+    ])
+    .factory('LocalStorage', ['$window', function($window) {
+        return {
+            set: function(key, value) {
+                $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+                return $window.localStorage[key] || defaultValue;
+            },
+            setObject: function(key, value) {
+                $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key) {
+                return JSON.parse($window.localStorage[key] || '{}');
+            }
+        }
+    }]);
