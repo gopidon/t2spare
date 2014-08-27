e55178ea08d15c70dfc5eb5f12b49bfc3e89c3c5
Gopi Donthireddy <gopi.don@gmail.com>
2014-08-27 21:10:12 +0530

    Add listing func

---
+++ trunk/client/www/js/controllers/listings.js
@@ -0,0 +1,14 @@
+/**
+ * Created by gopi on 8/27/14.
+ */
+angular.module('t2spare.listings',[])
+.controller('HomeCtrl',['$scope', function($scope){
+        $scope.hello = "Hello";
+
+    }])
+    .controller('MyListingsCtrl',['$scope', function($scope){
+
+    }])
+    .controller('MyListingDetailCtrl',['$scope', function($scope){
+
+    }])
